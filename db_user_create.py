7ba40917055d3155e40e2155603d173d3e8a477c
peps <pepsbox@gmail.com>
2015-04-25 06:04:02 +0300

    add useradd script and requerements.txt

---
+++ db_user_create.py
@@ -0,0 +1,14 @@
+from app import db, models
+
+print 'Set username:'
+nickname = raw_input()
+print 'Set pass for new user:'
+password = raw_input()
+print 'Set permissions. 0 - is user, 1 - is admin'
+role = raw_input()
+role = int(role)
+
+newuser = models.User(nickname = nickname, password = password, role = role)
+db.session.add(newuser)
+db.session.commit()
+print 'all is ok! User %s added.' % (nickname)
