3022ce67e24cd5f8ad340fc041371b97b8a8f8a8
peps <pepsbox@gmail.com>
2015-04-28 07:38:11 +0300

    Add moment.js for cionverting timestamps

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,13 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.bcrypt import Bcrypt
-#import views, models
+from momentjs import momentjs
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+app.jinja_env.globals['momentjs'] = momentjs
 
 lm = LoginManager()
 lm.init_app(app)
